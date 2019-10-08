be51febd9f91ba14a9d50c3ae7692cbda26b2273
CuongPV <pvcd03cntt@gmail.com>
2019-10-08 13:06:23 +0700

    function in tyscript

--- test.ts
+++ test.ts
@@ -35,3 +35,17 @@ let myVariable: any = 10;
 console.log(myVariable.name);
 myVariable();
 myVariable.toUpperCase();
+
+function add (num1: number,num2?: number):number{
+    if(num2)
+    return num1 + num2;
+    else return num1;
+
+}
+
+function add1 (num1: number,num2: number = 20) :number{
+    if(num2)
+    return num1 + num2;
+    else return num1;
+
+}
