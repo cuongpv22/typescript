4e5f0e3be4eebe5c304c796edd70085444fdec52
CuongPV <pvcd03cntt@gmail.com>
2019-10-17 15:14:04 +0700

    add

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import App from './app'
 import database from './database'
 
+//Start Server
 database();
 const app = new App();
 app.start();
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,10 @@
-import {Router,Request,Response} from 'express'
+import { Router, Request, Response } from 'express';
 
 const router = Router();
-router.get('/',(req: Request,res: Response) => {
+
+router.get('/', (req: Request, res: Response) => {
+    console.log('in router');
     res.render('index');
 });
+
 export default router;
